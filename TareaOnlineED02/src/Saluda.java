58c1f810335ccec923549192797da5f8d18c1886
claudia <lopezpinteno.claudia@gmail.com>
2016-11-26 19:41:56 +0100

    Commit programa nos pide nombre

--- TareaOnlineED02/src/Saluda.java
+++ TareaOnlineED02/src/Saluda.java
@@ -1,10 +1,19 @@
-
+import java.util.Scanner;
 public class Saluda {
 
+	public Saluda () {
+		
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println ("Hola holita vecinito");
+		Scanner sc = new Scanner (System.in);
+		String nombre;
+		System.out.println ("Por favor, introduce tu nombre");
+		nombre = sc.nextLine();
+		System.out.println ("Buenos días: "+ nombre);
 	}
 
 }
+
